c39ab396d6df154d16461bb2427127f64fb9a1b9
caioviniciusml <devcaioviniciusml@gmail.com>
2025-07-31 03:45:16 -0300

    feat: add fan control panel

--- src/components/fan-control.tsx
+++ src/components/fan-control.tsx
@@ -4,13 +4,13 @@ import { Fan } from "lucide-react"
 import { Switch } from "@/components/ui/switch"
 
 export function FanControl() {
-  const [autoMode, setAutoMode] = useState<boolean>(false)
+  const [autoMode, setAutoMode] = useState<boolean>(true)
   const [fanSpeed, setFanSpeed] = useState<number>(120)
 
   return (
     <section className="grid grid-cols-1 gap-5">
       <h2 className="text-2xl text-primary font-bold">FAN</h2>
-      <div className="px-5 py-10 flex flex-col md:flex-row gap-10 justify-between items-center text-secondary font-medium bg-soft rounded-xl shadow-md">
+      <div className="px-5 py-10 flex flex-col md:flex-row gap-10 justify-between items-center text-secondary font-medium bg-soft rounded-lg shadow-md">
         <div className="w-full flex items-center gap-2">
           <Fan />
           <span className="text-xl">Auto Mode</span>
@@ -20,15 +20,15 @@ export function FanControl() {
           <h3 className="text-xl">Speed</h3>
           <div className="flex gap-1 text-soft">
             <button
-              className={`w-full py-2 rounded-lg ${fanSpeed === 0 ? 'bg-primary' : 'bg-third'} ${autoMode ? 'hover:cursor-not-allowed opacity-60' : 'hover:cursor-pointer'}`}
+              className={`w-full py-2 rounded-lg cursor-pointer disabled:cursor-not-allowed disabled:opacity-60 ${fanSpeed === 0 ? 'bg-primary' : 'bg-third'}`}
               onClick={() => setFanSpeed(0)}
               disabled={autoMode}>I</button>
             <button
-              className={`w-full py-2 rounded-lg ${fanSpeed === 120 ? 'bg-primary' : 'bg-third'} ${autoMode ? 'hover:cursor-not-allowed opacity-60' : 'hover:cursor-pointer'}`}
+              className={`w-full py-2 rounded-lg cursor-pointer disabled:cursor-not-allowed disabled:opacity-60 ${fanSpeed === 120 ? 'bg-primary' : 'bg-third'}`}
               onClick={() => setFanSpeed(120)}
               disabled={autoMode}>II</button>
             <button
-              className={`w-full py-2 rounded-lg ${fanSpeed === 255 ? 'bg-primary' : 'bg-third'} ${autoMode ? 'hover:cursor-not-allowed opacity-60' : 'hover:cursor-pointer'}`}
+              className={`w-full py-2 rounded-lg cursor-pointer disabled:cursor-not-allowed disabled:opacity-60 ${fanSpeed === 255 ? 'bg-primary' : 'bg-third'} ${autoMode ? 'hover:cursor-not-allowed opacity-60' : 'hover:cursor-pointer'}`}
               onClick={() => setFanSpeed(255)}
               disabled={autoMode}>III</button>
           </div>
